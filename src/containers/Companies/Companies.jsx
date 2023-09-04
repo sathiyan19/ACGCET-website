a339bdc1bbe289da9be72abc6d66a4af31b070d9
ArjunPrakash22 <arjunprakashas03@gmail.com>
2023-09-04 20:23:38 +0530

    Merge branch 'main' of https://github.com/sathiyan19/ACGCET-website into arjun

--- src/containers/Companies/Companies.jsx
+++ src/containers/Companies/Companies.jsx
@@ -2,53 +2,42 @@ import React from "react";
 
 import "./Companies.css";
 
-import zoho from "../../assets/pictures/zoho.webp";
-import hcl from "../../assets/pictures/hcl.webp";
-import lt from "../../assets/pictures/lt.webp";
-import gofrugal from "../../assets/pictures/gofrugal.webp";
-import wipro from "../../assets/pictures/wipro.webp";
-import whiteblue from "../../assets/pictures/whiteblue.webp";
+
 import { Underline } from "../../widgets";
 
 
-
+import { reccarousal } from '../../constants/home';
 import { Slideshow } from '../../components';
+
 
 import q1 from "../../assets/pictures/quote1.png";
 import q2 from "../../assets/pictures/quote2.png";
-const reccarousal=[
-    zoho,
-    hcl,
-    lt,
-    gofrugal,
-    wipro,
-    whiteblue
-  ]
+
 const Companies = () => {
   return (
     <div className="rec">
-      <div className="recout1"></div>
-      <div className="recout2"></div>
-      <div className="recin">
-        <div className="rectext">
-          <Underline heading="Top Recruiters" />
-          <div className="recparasec">
-            <img className="recq1" src={q1} alt="Mottonss" />
-            <p className="recpara">
-              Experience our College's Top Recruiters Section, where excellence
-              meets opportunity. Explore the remarkable collaborations we've
-              fostered with leading companies, enabling our students to embark
-              on successful journeys and impactful careers in diverse fields."
-            </p>
-            <img className="recq2" src={q2} alt="Mottonss" />
+        <div className="recout1"></div>
+        <div className="recout2"></div>
+        <div className="recin">
+          <div className="rectext">
+            <Underline heading="Top Recruiters" />
+            <div className="recparasec">
+              <img className="recq1" src={q1} alt="Mottonss" />
+              <p className="recpara">
+                Experience our College's Top Recruiters Section, where
+                excellence meets opportunity. Explore the remarkable
+                collaborations we've fostered with leading companies, enabling
+                our students to embark on successful journeys and impactful
+                careers in diverse fields."
+              </p>
+              <img className="recq2" src={q2} alt="Mottonss" />
+            </div>
+          </div>
+          <div className="recpic">
+            <Slideshow images={reccarousal} flag="true" />
           </div>
         </div>
-        <div className="recpic">
-          <Slideshow images={reccarousal} flag="true"/>
-        </div>
-      </div>
     </div>
   );
 };
-
 export default Companies;
