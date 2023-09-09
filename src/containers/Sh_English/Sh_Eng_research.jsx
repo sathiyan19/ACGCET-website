26c501be68f36705e23d1cf14ec1e9e90e569e04
ArjunPrakash22 <arjunprakashas03@gmail.com>
2023-09-09 19:00:41 +0530

    added research pages in science and humanities

--- src/containers/Sh_English/Sh_Eng_research.jsx
+++ src/containers/Sh_English/Sh_Eng_research.jsx
@@ -9,14 +9,14 @@ import { Radial_menu } from "../../components";
 import { Sh_dept_fmenu } from '../../constants/Sh_dept_menu';
 
 
-const Sh_mat_lab = () => {
+const Sh_Eng_research = () => {
   return (
     <div>
-      <section className="shmatcons">
-      <div className='shmatbg'>
+      <section className="shengcons">
+      <div className='shengbg'>
         <img className='uct_bg' src={under} alt='under_construction'/>
       </div>
-      <div className='shmatbg'>
+      <div className='shengbg'>
         <img className='uct_bg_mob' src={underm} alt='under_constructionp'/>
       </div>
         
@@ -28,4 +28,4 @@ const Sh_mat_lab = () => {
   )
 }
 
-export default Sh_mat_lab
+export default Sh_Eng_research
