869c510c521bab5b1fd6a295641d58552a3d5a38
KaviyaB912 <kaviyab912@gmail.com>
2023-09-12 11:31:45 +0530

    changes in hod msg

--- src/constants/thiran.js
+++ src/constants/thiran.js
@@ -3,8 +3,19 @@ import image1 from "../assets/pictures/nsshero1.webp";
 import image2 from "../assets/pictures/nsshero2.webp";
 import image3 from "../assets/pictures/nsshero3.webp";
 import image4 from "../assets/pictures/nsshero4.webp";
-import kavz from "../assets/pictures/girl.webp";
-import cov from "../assets/pictures/csecover.webp";
+
+import cover from "../assets/pictures/pangu auto.webp";
+import cover1 from "../assets/pictures/thiran_water.webp";
+import cover2 from "../assets/pictures/thiran_home.webp";
+import cover3 from "../assets/pictures/thiran_driver.webp";
+import cover4 from "../assets/pictures/compass.webp";
+import cover5 from "../assets/pictures/thiran_civil.webp";
+
+
+
+
+
+
 
 
 
@@ -15,13 +26,13 @@ const Thiran_heroimage_carousalinfo = {
     img_list: [image1, image2 ,image3,image4],
   };
 
-  const vision= {
+const vision= {
 
     id:'1',
     visiondata:'To enhance the knowledge of an individual in particular concern that is considered and improve the leadership skills  of an every individual by a set of achievable actions.'
-  }
+  };
 
-  const mission_data=[
+const mission_data=[
     {
       id:'1',
       mission:'To provide the standard approach for the porject execution.'
@@ -41,30 +52,65 @@ const Thiran_heroimage_carousalinfo = {
 
   ]
 
-  const profile_info =[
-    {
-        'id':"1",
-        'cover':cov,
-        'dp':kavz,
-        'name':"Kaviya",
-        'Designation':"IAS Officer",
-        'mailid':"abc@gmail.com"
-    },
-    {
-      'id':"2",
-      'cover':cov,
-      'dp':kavz,
-      'name':"Yogesh",
-      'Designation':"Doctor",
-      'mailid':"abc@gmail.com"
+
+
+const jury_awards=[
+  {
+      id:'1',
+      coverpic:cover,
+      batch:"2021-25 Batch",
+      name:"Share Auto App",
+      desc:"Sathiyan and Harish are the ones who have brought this project to fruition.",
+      date:"24th July 2023"
   },
   {
-    'id':"3",
-    'cover':cov,
-    'dp':kavz,
-    'name':"Hema",
-    'Designation':"Doctor",
-    'mailid':"abc@gmail.com"
-}
-  ]
-export{ Thiran_heroimage_carousalinfo,vision,mission_data,profile_info}
+    id:'2',
+    coverpic:cover1,
+    batch:"2020-24 Batch",
+    name:"Smart Public Water Distribution System",
+    desc:"K.Madhavan,S.Vinothkumar,M.M.Nidhish",
+    date:"24th July 2023"
+  },
+  {
+    id:'3',
+    coverpic:cover2,
+    batch:"1994-98",
+    name:"Harmony '98",
+    desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    date:"24th July 2023"
+  },
+  {
+    id:'4',
+    coverpic:cover3,
+    batch:"1994-98",
+    name:"Harmony '98",
+    desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    date:"24th July 2023"
+  },
+  {
+    id:'5',
+    coverpic:cover4,
+    batch:"1994-98",
+    name:"Harmony '98",
+    desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    date:"24th July 2023"
+  },
+  {
+    id:'6',
+    coverpic:cover5,
+    batch:"1994-98",
+    name:"Harmony '98",
+    desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    date:"24th July 2023"
+  },
+  {
+    id:'7',
+    coverpic:cover,
+    batch:"1994-98",
+    name:"Harmony '98",
+    desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    date:"24th July 2023"
+  }
+]
+
+export{ Thiran_heroimage_carousalinfo,vision,mission_data,jury_awards}
