62593142d53453ea93b0cf46f79d015f9abce3e6
ArjunPrakash22 <arjunprakashas03@gmail.com>
2023-09-14 03:37:25 +0530

    added civil dept

--- src/App.js
+++ src/App.js
@@ -81,7 +81,9 @@ import {
   Ece_research,
   Devpage,
   Eee_research,
-  Eee_nextgen
+  Eee_nextgen,
+  Civil_internal,
+  Civil_research
 } from "./pages";
 import "./App.css";
 import Uba from "./pages/Uba/Uba";
@@ -150,7 +152,9 @@ const App = () => {
         <Route path="/civildept/civil-peo" element={<Civil_peo/>} />
         <Route path="/civildept/civil-faculties" element={<Civil_faculties/>} />
         <Route path="/civildept/civil-lab" element={<Civil_lab/>} />
+        <Route path="/civildept/civil-research" element={<Civil_research/>} />
         <Route path="/civildept/civil-dept-events" element={<Civil_events/>} />
+        <Route path="/civildept/civil-internal-revenue" element={<Civil_internal/>} />
         <Route path="/mechdept/mech-peo" element={<Mech_peo/>} />
         <Route path="/mechdept/mech-faculties" element={<Mech_faculties/>} />
         <Route path="/mechdept/mech-lab" element={<Mech_lab/>} />
