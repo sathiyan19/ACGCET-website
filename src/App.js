9f6e087dff0bcfdc5ba3e91e284a3e1c1d4fe655
ArjunPrakash22 <arjunprakashas03@gmail.com>
2023-09-13 17:14:09 +0530

    nextgen and lab page in eee and float in civil

--- src/App.js
+++ src/App.js
@@ -78,7 +78,8 @@ import {
   Civil_awards,
   Ece_dept_lib,
   Ece_research,
-  Eee_research
+  Eee_research,
+  Eee_nextgen
 } from "./pages";
 import "./App.css";
 import Uba from "./pages/Uba/Uba";
@@ -155,6 +156,7 @@ const App = () => {
         <Route path="/eeedept/eee-faculties" element={<Eee_faculties/>} />
         <Route path="/eeedept/eee-lab" element={<Eee_lab/>} />
         <Route path="/eeedept/eee-research" element={<Eee_research/>} />
+        <Route path="/eeedept/eee-nextgen" element={<Eee_nextgen/>}/>
         <Route path="/eeedept/eee-dept-events" element={<Eee_events/>} />
         <Route path="/uba" element={<Uba/>} />
         <Route path="/ecedept/ece-placement" element={<Ece_placement/>} />
