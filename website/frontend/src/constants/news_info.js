868de77b7ff7b7cccf37828b2d38be2f29de1076
Sathiyan R <sathiyanr2003@gmail.com>
2024-01-26 22:04:03 +0530

    Made some adjustments-sathiyan

--- website/frontend/src/constants/news_info.js
+++ website/frontend/src/constants/news_info.js
@@ -23,6 +23,22 @@ export const news_info=[
     //     'content_2':"Entrepreneurship Development is defined as the process of improving entrepreneurs’ skills and knowledge in the development, management, and organisation of a business venture while keeping the risks in mind. On 31.8.23, the entrepreneurship development cell of our college held a selection of coordinators from third years in our college’s gallery hall.Many entrepreneur enthusiasts took part actively."
     // },
     {
+        'id':'7',
+        'heading':"Envision Conference at IITMRP",
+        'sub-head':'Empowering Tomorrow\'s Leaders: Insights from the Envision Energy Conference at IITMRP',
+        'pic':envision,
+        // 'content_1':'A set of nine 2nd year students of our college recently had the opportunity to attend a dynamic conference themed "Envision Energy Conference" at IITMRP, on the dates 4th and 5th January, 2024, through the build club, which is being a great source of immense opportunities for our students. The conference proved to be an enlightening experience, that brought together professionals from diverse fields to exchange insights and ideas. The keynote sessions were particularly captivating, featuring industrial and educational delegates who shared their captivating and futuristic ideas. The interactive panel discussions were a highlight, allowing experts to engage in thought-provoking conversations. This groundbreaking conference not only provided valuable exposure to visionary ideas but also served as a platform for our students to broaden their understanding of the dynamic world around them',
+        'content_2':'A set of nine 2nd year students of our college recently had the opportunity to attend a dynamic conference themed "Envision Energy Conference" at IITMRP, on the dates 4th and 5th January, 2024, through the build club, which is being a great source of immense opportunities for our students. The conference proved to be an enlightening experience, that brought together professionals from diverse fields to exchange insights and ideas. The keynote sessions were particularly captivating, featuring industrial and educational delegates who shared their captivating and futuristic ideas. The interactive panel discussions were a highlight, allowing experts to engage in thought-provoking conversations. This groundbreaking conference not only provided valuable exposure to visionary ideas but also served as a platform for our students to broaden their understanding of the dynamic world around them.'
+    },
+    {
+        'id':'8',
+        'heading':" SIH winner",
+        'sub-head':'Engineering Excellence: Triumph at Smart India Hackathon Hardware Edition 2023',
+        'pic':sih,
+        'content_1':'From our college , 5 students of mechanical engineering and 1 student of Electrical and Electronics Engineering ,3 rd years  won   prize in Smart India hackathon hardware edition 2023 conducted at  Nalla malla Reddy Engineering College , Hyderabad  from dec 19 to 23 ,2023 .',
+        'content_2':'The 14.6 km long cable belt conveyor is supported on pulleys. A steel rope runs over these pulleys while the belt sits over them. The conveyor is operated by pulling the steel ropes with a drive motor. The rope gets elongated due to pulling force leading to breakage of its strands. Similarly, the belt also gets worn out causing stoppage of conveyor operation unpredictably. Solution Desired: By capturing the past reasons for wear and tear including visuals and using a suitable ML application, try to predict the condition of the Rope and belt well in advance such that corrective and preventive actions can be taken before its failure preventing loss of production.'
+    },
+    {
         'id':'6',
         'heading':"General Aptitude Training",
         'sub-head':'Specialized weekend aptitude program for 60 third year students for academic & career success.',
@@ -38,47 +54,5 @@ export const news_info=[
         'content_1':'Alagappa Chettiar Government College of Engineering and Technology, Sivaganga, hosts diverse events: writing, essay competitions, quizzes, debates, and nukkad natak. Winners eagerly await felicitation on World Standards Day in Madurai, a celebration of academic excellence and achievement.',
         'content_2':"A shared vision for a better world embodies the collective aspirations of individuals, communities, and nations to create a more equitable, sustainable, and harmonious global society. It is a beacon of hope that unites people across diverse backgrounds, inspiring collaboration and action. This vision encompasses ideals such as social justice, environmental stewardship, and peace, fostering a sense of responsibility to leave a lasting positive impact for future generations. It is a powerful force that drives humanity toward a brighter and more inclusive future."
     },
-    {
-        'id':'1',
-        'heading':"Envision Energy Conference at IITMRP",
-        'sub-head':'Empowering Tomorrow\'s Leaders: Insights from the Envision Energy Conference at IITMRP',
-        'pic':envision,
-        // 'content_1':'A set of nine 2nd year students of our college recently had the opportunity to attend a dynamic conference themed "Envision Energy Conference" at IITMRP, on the dates 4th and 5th January, 2024, through the build club, which is being a great source of immense opportunities for our students. The conference proved to be an enlightening experience, that brought together professionals from diverse fields to exchange insights and ideas. The keynote sessions were particularly captivating, featuring industrial and educational delegates who shared their captivating and futuristic ideas. The interactive panel discussions were a highlight, allowing experts to engage in thought-provoking conversations. This groundbreaking conference not only provided valuable exposure to visionary ideas but also served as a platform for our students to broaden their understanding of the dynamic world around them',
-        'content_2':'A set of nine 2nd year students of our college recently had the opportunity to attend a dynamic conference themed "Envision Energy Conference" at IITMRP, on the dates 4th and 5th January, 2024, through the build club, which is being a great source of immense opportunities for our students. The conference proved to be an enlightening experience, that brought together professionals from diverse fields to exchange insights and ideas. The keynote sessions were particularly captivating, featuring industrial and educational delegates who shared their captivating and futuristic ideas. The interactive panel discussions were a highlight, allowing experts to engage in thought-provoking conversations. This groundbreaking conference not only provided valuable exposure to visionary ideas but also served as a platform for our students to broaden their understanding of the dynamic world around them.'
-    },
-    {
-        'id':'1',
-        'heading':" SIH winner",
-        'sub-head':'Engineering Excellence: Triumph at Smart India Hackathon Hardware Edition 2023',
-        'pic':sih,
-        'content_1':'From our college , 5 students of mechanical engineering and 1 student of Electrical and Electronics Engineering ,3 rd years  won   prize in Smart India hackathon hardware edition 2023 conducted at  Nalla malla Reddy Engineering College , Hyderabad  from dec 19 to 23 ,2023 .',
-        'content_2':'The 14.6 km long cable belt conveyor is supported on pulleys. A steel rope runs over these pulleys while the belt sits over them. The conveyor is operated by pulling the steel ropes with a drive motor. The rope gets elongated due to pulling force leading to breakage of its strands. Similarly, the belt also gets worn out causing stoppage of conveyor operation unpredictably. Solution Desired: By capturing the past reasons for wear and tear including visuals and using a suitable ML application, try to predict the condition of the Rope and belt well in advance such that corrective and preventive actions can be taken before its failure preventing loss of production.'
-    },
-
-
-    // {
-    //     'id':'2',
-    //     'heading':'Placement Activities',
-    //     'sub-head':'Some representative placeholder content for the second slide of the carousel.',
-    //     'pic':mechcover,
-    //     'content_1':'Our Commitment as a Centre of Engineering Education is to impart Technical Knowledge par excellence, motivate the learners in Research, evolve result-oriented, innovative techniques in Engineering, provide necessary career guidance, and train our learners'+
-    //                 'in leadership qualities so as to achieve better productivity and prosperity for our country.',
-    //     'content_2':"ACGCET, a leader in higher education in engineering and technology, is committed to excellence through the delivery of outstanding undergraduate and graduate education that remains current and responsive to the diverse needs of those it serves. The institution’s"+
-    //     "faculty and staff promote the intellectual, professional, social, and personal development of its students through innovations in learning, scholarship, and creative endeavours. Students, faculty, and staff partner to create strong"+
-    //     "engagement with the local, national, and global communities. The institution dedicates itself to academic vitality, integrity, and diversity."
-    // },
-    // {
-    //     'id':'3',
-    //     'heading':'Foreign Training at University',
-    //     'sub-head':'Some representative placeholder content for the second slide of the carousel.',
-    //     'pic':eleccover,
-    //     'content_1':'Our Commitment as a Centre of Engineering Education is to impart Technical Knowledge par excellence, motivate the learners in Research, evolve result-oriented, innovative techniques in Engineering, provide necessary career guidance, and train our learners'+
-    //                 'in leadership qualities so as to achieve better productivity and prosperity for our country.',
-    //     'content_2':"ACGCET, a leader in higher education in engineering and technology, is committed to excellence through the delivery of outstanding undergraduate and graduate education that remains current and responsive to the diverse needs of those it serves. The institution’s"+
-    //     "faculty and staff promote the intellectual, professional, social, and personal development of its students through innovations in learning, scholarship, and creative endeavours. Students, faculty, and staff partner to create strong"+
-    //     "engagement with the local, national, and global communities. The institution dedicates itself to academic vitality, integrity, and diversity."
-    // },
-    
-    
     
 ]
