01838e99514a950f0a34823979f34b75bd5aca2c
BELCHARLES <arockiyabelcyvs@gmail.com>
2024-03-10 10:21:49 +0530

    getting data

--- website/frontend/src/constants/path.js
+++ website/frontend/src/constants/path.js
@@ -299,13 +299,13 @@ const bottom_nav_paths = [
   },
 ];
 const login_nav_paths = [
-  {
-    id: "20",
-    type: "4",
-    title: "",
-    basepath: "/dashboard",
-    links: [],
-  },
+  // {
+  //   id: "20",
+  //   type: "4",
+  //   title: "",
+  //   basepath: "/dashboard",
+  //   links: [],
+  // },
 ];
 
 export { top_nav_paths, bottom_nav_paths, hidden_nav_paths, login_nav_paths };
