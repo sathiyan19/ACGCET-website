ae027127d36ce18f2bea60c574a83b9e66af2682
ArjunPrakash22 <arjunprakashas03@gmail.com>
2024-04-26 11:39:13 +0530

    .

--- website/frontend/src/pages/Dashboard/Dashboard.jsx
+++ website/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -453,7 +453,7 @@ const Dashboard = () => {
                   <Table data={results} columns={column}/>
                   <div class="marksheet-container">
     <div className="cool_holder">
-    <button onClick={download_marksheet} className="coolass_button" id="coolbutton"></button>
+    {/* <button onClick={download_marksheet} className="coolass_button" id="coolbutton"></button> */}
   </div>
   </div>
                   
