bb55f0aa5b210776dd9c7b30b2141d30338abcb4
ArjunPrakash22 <arjunprakashas03@gmail.com>
2024-04-29 20:44:23 +0530

    .

--- website/backend/main_app/marksheet_template.js
+++ website/backend/main_app/marksheet_template.js
@@ -6,7 +6,7 @@ global.appRoot = path.resolve(__dirname);
 
 const subjects_list = require('./subjects');
 
-const pdftemplate = ({ studentname, stud_dept, dob, reg_no, batch, gender, cgpa, sem_no, sem_subs, bg }) => {
+const pdftemplate = ({ studentname, stud_dept, dob, reg_no, batch, gender, cgpa, sem_no, sem_subs }) => {
   const today = new Date();
   const sub_dept = stud_dept + '_subs';
   var tableRows = '';
