d58edbf607bcd49a395a635f429e26efc0f7804a
BELCHARLES <arockiyabelcyvs@gmail.com>
2024-08-08 16:29:52 +0530

    feedback

--- website/frontend/src/constants/path.js
+++ website/frontend/src/constants/path.js
@@ -34,10 +34,10 @@ const top_nav_paths = [
       { path: "/programs", link: "Programs offered" },
       { path: "/academic-calendar", link: "Academic Calendar" },
       {path:"coe",link:"Autonomous Office"},
-      {path:"/research",link:"Research"},
+      // {path:"/research",link:"Research"},
       // {path:"/feedback/alumini_fb",link:"Research"},
-
-      // {path:"/feedback/collaborator_fb",link:"Feedback"},
+      {path:"/feedback/supplier_fb",link:"Research"},
+      {path:"/feedback/employer_fb",link:"Feedback"},
       // {path:"/sgpa",link: "Sgpa Calculator"}
     ],
   },
