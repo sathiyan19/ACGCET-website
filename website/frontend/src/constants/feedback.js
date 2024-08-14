df43f1ee566382f1bdc85a4ae8ad0ab76245236e
Mohanamala07 <mohanamala07@gmail.com>
2024-08-14 10:29:48 +0530

    feedback

---
+++ website/frontend/src/constants/feedback.js
@@ -0,0 +1,89 @@
+// ../../constants/feedback.js
+import graduation from "../assets/pictures/graduation.jpg";
+import feedback2 from "../assets/pictures/feedback2.jpg";
+import feedback3 from "../assets/pictures/feedback3.png";
+import feedback4 from "../assets/pictures/feedback4.png";
+import aluminiimage from "../assets/pictures/aluminiimage.jpg";
+import studentimg from "../assets/pictures/studentimg.jpeg";
+import star from "../assets/pictures/star.jpeg";
+export const cardData = [
+  {
+    id: '1',
+    description: 'Student ',
+    color: '#1e73be',
+    backgroundImage: aluminiimage,
+    path: '/local-info',
+    // gridColumn: 'span 2',
+    // gridRow: 'span 1',
+  },
+  {
+    id: '1',
+    description: 'Student ',
+    color: '#1e73be',
+    backgroundImage: aluminiimage,
+    path: '/local-info',
+    // gridColumn: 'span 1',
+    // gridRow: 'span 2',
+  },
+  {
+    id: '1',
+    description: 'Student ',
+    color: '#1e73be',
+    backgroundImage: graduation,
+    path: '/local-info',
+   
+    // gridColumn: 'span 2',
+    // gridRow: 'span 1',
+  },
+  {
+    id: '1',
+    description: 'Student ',
+    color: '#1e73be',
+    backgroundImage: aluminiimage,
+    path: '/local-info',
+   
+    // gridColumn: 'span 1',
+    // gridRow: 'span 2',
+  },
+   {
+    id: '1',
+    description: 'Student feedback',
+    color: '#1e73be',
+    backgroundImage: feedback2,
+    path: '/local-info',
+   
+    // gridColumn: 'span 2',
+    // gridRow: 'span 1',
+  },
+  {
+    id: '1',
+    description: 'Student feedback',
+    color: '#1e73be',
+    backgroundImage: feedback2,
+    path: '/local-info',
+    
+    // gridColumn: 'span 1',
+    // gridRow: 'span 2',
+  },
+  {
+    id: '1',
+    description: 'Student feedback',
+    color: '#1e73be',
+    backgroundImage: feedback2,
+    path: '/local-info',
+   
+    // gridColumn: 'span 2',
+    // gridRow: 'span 1',
+  },
+  {
+    id: '1',
+    description: 'Student feedback',
+    color: '#1e73be',
+    backgroundImage: feedback2,
+    path: '/local-info',
+   
+    // gridColumn: 'span 1',
+    // gridRow: 'span 2',
+  },
+ 
+];
--- website/frontend/src/constants/path.js
+++ website/frontend/src/constants/path.js
@@ -35,7 +35,7 @@ const top_nav_paths = [
       { path: "/academic-calendar", link: "Academic Calendar" },
       {path:"/coe",link:"Autonomous Office"},
       {path:"/research",link:"Research"},
-      // {path:"/feedback",link:"Feedback"},
+      {path:"/feedback",link:"Feedback"},
       // {path:"/sgpa",link: "Sgpa Calculator"}
     ],
   },
