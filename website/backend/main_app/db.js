7feab1ae396b4407f7d282f77202dc22505d9f30
kanishkamohan2411 <kanishkamnkl@gmail.com>
2024-09-28 09:39:58 +0530

    change

--- website/backend/main_app/db.js
+++ website/backend/main_app/db.js
@@ -5,10 +5,10 @@ const dbConfig = {
     user: 'root',
     // password: 'kint',
     // password: 'Sep14@23',
-    password:'Mohana@07',
+    // password:'Mohana@07',
     // password: 'Badminton19@',
     // password: 'kaviya',
-    // password: 'root',
+    password: 'root',
     // password:'1988',
     database: 'clgdata'
   };
